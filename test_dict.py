a55d438b511d76fcbfddda8d8008a9dabf25fa88
Ella Yakir <ellayakir@mail.tau.ac.il>
2024-08-20 12:48:34 +0300

    update

--- test_dict.py
+++
@@ -1,4 +0,0 @@
-my_dict = {'paradigm': 'RSVP', 'presentation': 'Center', 'word_presentation_duration': 300,
-           'inter_word_break_duration': 100, 'context_choice': 'Yes', 'trials_before_break': 10,
-           'context_sentence_interval': 100, 'completion_text': 'תודה על ההשתתפות!', 'break_screen_text':
-           'זה הזמן לקחת הפסקה קצרה. לחזרה, יש ללחוץ על מקש הרווח'}

6f23a6d06cd6c29658f87a1fca0a102f262a4974
Ella Yakir <ellayakir@mail.tau.ac.il>
2024-08-20 12:15:41 +0300

    update

--- loading_stuff.py
+++ loading_stuff.py
@@ -7,9 +7,72 @@ def load_scripts():
         rsvp_moving_window = f.read()
     with open('spr_moving_window.txt', 'r') as f:
         spr_moving_script = f.read()
-    scripts = [spr_moving_script, spr_centered_script,rsvp_centered_script, rsvp_moving_window]
+    scripts = [spr_moving_script, spr_centered_script, rsvp_moving_window, rsvp_centered_script]
 
     return scripts
+
+
+def retrieve_script(gui_output: dict):
+    scripts = load_scripts()
+    if gui_output['paradigm'] == 'SPR':
+        if gui_output['display_config'] == 'Moving window':
+            my_script = scripts[0]
+        elif gui_output['display_config'] == 'Centered':
+            my_script = scripts[1]
+    elif gui_output['paradigm'] == 'RSVP':
+        if gui_output['display_config'] == 'Moving window':
+            my_script = scripts[2]
+        elif gui_output['display_config'] == 'Centered':
+            my_script = scripts[3]
+
+    return my_script
+
+
+def custom_parameters_assigment(gui_output: dict):
+    custom_parameters = {}
+
+    if gui_output['paradigm'] == 'RSVP':
+        custom_parameters['word_duration'] = gui_output['presentation_duration']
+        custom_parameters['break_duration'] = gui_output['inter_word_break_duration']
+
+    if gui_output['context_choice'] == 'Yes':
+        context_break_duration = gui_output['context_sentence_interval']
+        custom_parameters['context_break_duration'] = context_break_duration
+
+    if gui_output['trials_until_break'] != 0:
+        custom_parameters['trials_until_break'] = gui_output['trials_until_break']
+
+    custom_parameters = {**custom_parameters, 'completion_text': gui_output['completion_text'],
+                         'break_screen_text': gui_output['break_screen_text'],
+                         'practice_end_text': gui_output['practice_end_text']}
+    return custom_parameters
+
+
+def customize_script(gui_output: dict):
+    custom_parameters = custom_parameters_assigment(gui_output)
+    # Initialize an empty list to hold each line
+    lines = []
+
+    # Iterate over the dictionary and format each key-value pair
+    for key, value in custom_parameters.items():
+        if isinstance(value, str):
+            # Format string values with quotes
+            line = f'{key} = "{value}"'
+        else:
+            # Format non-string values directly
+            line = f'{key} = {value}'
+
+        # Append the formatted line to the list
+        lines.append(line)
+
+    # Join all lines into a single string with newline characters
+    variables_assignment_string = '\n'.join(lines)
+
+    # Join lines to the script
+    my_script = retrieve_script(gui_output)
+    my_script = variables_assignment_string + '\n' + my_script
+
+    return my_script
 
 
 def load_stimuli(experimental_stimuli, practice_stimuli):
@@ -23,14 +86,3 @@ def load_stimuli(experimental_stimuli, practice_stimuli):
 
     return stimuli
 
-
-experimental_stimuli = 'demo_items.csv'
-practice_stimuli = 'practice_stimuli.csv'
-word_duration = 300
-break_duration = 100
-presentation_manner = 'moving'
-rsvp_demo_dict = {'paradigm': 'rsvp', 'experimental_stimuli': experimental_stimuli, 'practice_stimuli':practice_stimuli,
-                  'word duration': word_duration, 'break duration': break_duration,
-                  'presentation manner': presentation_manner}
-
-a=1

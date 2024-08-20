1fa7994b1b5e5b58e9f4ff6614552b0e336634ba
Ella Yakir <ellayakir@mail.tau.ac.il>
2024-08-20 12:50:40 +0300

    update

--- testing_dict.py
+++ testing_dict.py
@@ -1,5 +1,5 @@
 my_dict = {'paradigm': 'RSVP', 'display_config': 'Centered', 'word_presentation_duration': 300,
-           'inter_word_break_duration': 100, 'context_choice': 'Yes', 'trials_before_break': 10,
+           'inter_word_break_duration': 100, 'context_choice': 'Yes', 'trials_until_break': 10,
            'context_sentence_interval': 100, 'completion_text': 'תודה על ההשתתפות!', 'break_screen_text':
            'זה הזמן לקחת הפסקה קצרה. לחזרה, יש ללחוץ על מקש הרווח', 'practice_end_text':
                'שלב האימון הסתיים. להמשך לניסוי, יש ללחוץ על מקש הרווח.'}
@@ -7,5 +7,6 @@ my_dict = {'paradigm': 'RSVP', 'display_config': 'Centered', 'word_presentation_
 from loading_stuff import *
 
 fff = retrieve_script(my_dict)
+b = custom_parameters_assigment(my_dict)
 a = 1
 

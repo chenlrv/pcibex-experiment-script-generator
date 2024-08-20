fa57a81c7be20a6a5bc8e92c3bfbc019f4ef3a00
Ella Yakir <ellayakir@mail.tau.ac.il>
2024-08-20 21:44:50 +0300

    .

--- testing_dict.py
+++ testing_dict.py
@@ -1,11 +1,22 @@
 from loading_stuff import *
 
-my_dict = {'paradigm': 'RSVP', 'display_config': 'Centered', 'word_presentation_duration': 300,
-           'inter_word_break_duration': 100, 'context_choice': 'No', 'trials_until_break': 0,
-           'context_sentence_interval': 100,'question_type': 'yes/no', 'completion_text': 'תודה על ההשתתפות!', 'break_screen_text':
-           'זה הזמן לקחת הפסקה קצרה. לחזרה, יש ללחוץ על מקש הרווח', 'practice_end_text':
-               'שלב האימון הסתיים. להמשך לניסוי, יש ללחוץ על מקש הרווח.'}
-
+my_dict = {
+    "paradigm": "SPR",
+    "display_config": 'Moving window',
+    "files": {
+        "practice_file": "practice_file.csv",
+        "experiment_file": "experiment_file.csv"
+    },
+    "sections": {
+        "context": "Yes",
+        "context_sentence_interval": 1500,
+        "completion_screen_text": "Thank you for participating!",
+        "practice_end_text": "You have completed the practice session.",
+        "break_screen_text": "Take a short break.",
+        "trials_before_breaks": 20,
+        "answers": "Custom",
+    }
+}
 
 b = write_script(my_dict)
 
